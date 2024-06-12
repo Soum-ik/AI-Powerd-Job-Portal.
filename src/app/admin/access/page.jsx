ab698cec26e5e05f29f886ac9ccaa9e14b3b70cb
Stack Soumik <sarkarsoumik215@gmail.com>
2024-06-13 03:28:32 +0600

    fix: template litarels issue

--- src/app/admin/access/page.jsx
+++ src/app/admin/access/page.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+ 
 
 function page() {
   return (
     <div className="mx-auto my-20 max-w-5xl p-3">
       <h2 className="text-center text-3xl font-semibold text-neutral-800 ">
-        You're are not eligible for this route
+        {`You're are not eligible for this route`}
       </h2>
     </div>
   );
